78eafb780d8089c335081875bc02d33dc53ff365
Rafał Wołejko <czakalaka25@gmail.com>
2023-06-01 18:51:23 +0200

    initialize repo

---
+++ src/main/java/com/example/itconferenceservice/ItConferenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.itconferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConferenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItConferenceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itconferenceservice/ItConferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itconferenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
