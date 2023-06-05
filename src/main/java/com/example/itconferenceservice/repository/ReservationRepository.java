e9f4e1d8c3a73758330d64b756b8420f92eb0e2f
Rafał Wołejko <czakalaka25@gmail.com>
2023-06-05 19:48:43 +0200

    cleanup

--- src/main/java/com/example/itconferenceservice/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.itconferenceservice.repository;
-
-import com.example.itconferenceservice.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
-}
